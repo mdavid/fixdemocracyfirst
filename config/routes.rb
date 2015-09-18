f246fbebe1acc3199bcb4d25fbbd7889f89d47e6
Chris Bucchere <chris@bucchere.com>
2015-09-18 15:37:33 -0700

    cruft removal

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'search' => 'search#search'
   post 'user/social_auth' => 'users#social_auth'
-  resources :users
   get 'statements/browse' => 'statements#browse'
+  resources :users
   resources :statements
   resources :people
   resources :candidates
@@ -20,7 +20,6 @@ Rails.application.routes.draw do
   mount ResqueWeb::Engine => "/resque_web"
   get 'admin' => 'static_pages#admin'
   get 'attributions' => 'static_pages#attributions'
-  get 'ask_the_question_guide' => 'static_pages#ask_the_question_guide'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
