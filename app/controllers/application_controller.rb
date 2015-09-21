71365d2ac62329b06c98a3ed3acc499c23f5bd50
Chris Bucchere <chris@bucchere.com>
2015-09-20 17:58:28 -0700

    temporarily disables admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,6 @@ class ApplicationController < ActionController::Base
   end
   
   def require_admin
-    redirect_to '/' unless @current_user.admin?
+    #redirect_to '/' unless @current_user.admin?
   end
 end
